de55231e2f959bf37ac83bb30168911f9e046072
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-03-25 21:21:02 +0100

    Updated bwtool

--- install_bwtool.sh
+++ install_bwtool.sh
@@ -1,12 +1,12 @@
 git clone https://github.com/CRG-Barcelona/libbeato.git
 git clone https://github.com/CRG-Barcelona/bwtool.git
 cd libbeato/
-git checkout 0c30432
-./configure --prefix=$HOME CFLAGS="-g -O0 -I${HOME}/include" LDFLAGS=-L${HOME}/lib > /dev/null 2>&1
-make > /dev/null 2>&1
-make install > /dev/null 2>&1
+git checkout 0c30432 
+./configure --prefix=$HOME CFLAGS="-g -O0 -I${HOME}/include" LDFLAGS=-L${HOME}/lib
+make
+make install
 cd ../bwtool/
-./configure --prefix=$HOME CFLAGS="-g -O0 -I${HOME}/include" LDFLAGS=-L${HOME}/lib > /dev/null 2>&1
-make > /dev/null 2>&1
-make install > /dev/null 2>&1
+./configure --prefix=$HOME CFLAGS="-g -O0 -I${HOME}/include" LDFLAGS=-L${HOME}/lib
+make
+make install
 cd ..
